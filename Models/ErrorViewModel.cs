5219896301f9277553bfbdf8c833eff9be28e1b8
bfljpg <16008121008@ogr.bozok.edu.tr>
2024-12-12 11:37:21 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace online_is_bulma_platformu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
