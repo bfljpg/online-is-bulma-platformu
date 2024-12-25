7ada485dd331088a8441d0bda806f6ae26aba939
bfljpg <16008121008@ogr.bozok.edu.tr>
2024-12-25 21:35:43 +0300

    fix entity framework conflicts again

--- Data/JobPortalContext.cs
+++ Data/JobPortalContext.cs
@@ -1,12 +1,13 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.EntityFrameworkCore;
 using online_is_bulma_platformu.Models;
 
 namespace online_is_bulma_platformu.Data
 {
     public class JobPortalContext : DbContext
     {
-        // Bağlantı dizesi doğrudan buraya yazılıyor
-        public JobPortalContext() : base("Server=DESKTOP-SBTPIC1\\SQLEXPRESS;Database=JobPortalDB;Trusted_Connection=True;") { }
+        public JobPortalContext(DbContextOptions<JobPortalContext> options) : base(options)
+        {
+        }
 
         public DbSet<User> Users { get; set; }
         public DbSet<JobListing> JobListings { get; set; }
